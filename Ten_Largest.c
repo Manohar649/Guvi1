098fa1b0c25ef82e0e3db7d47afdd31aeaa1a1cf
Manohar649 <37975082+Manohar649@users.noreply.github.com>
2019-03-20 23:17:23 +0530

    Create Ten_Largest.c

---
+++ Ten_Largest.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+   int a[100],d,i,j,k,n,big;
+   scanf("%d",&n);
+   for(i=0;i<n;i++){
+   scanf("%d",&a[i]);
+   }
+       big=a[0];
+       for(i=1;i<n;i++){
+           if(big<a[i]){
+               big=a[i];
+           }
+       }
+   printf("%d",big);
+return 0;
+}
