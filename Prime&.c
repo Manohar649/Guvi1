44328d4080728f0edc6b575aa6ffb10b1d10ec8d
Manohar649 <37975082+Manohar649@users.noreply.github.com>
2019-03-24 18:33:53 +0530

    Create Prime&.c

---
+++ Prime&.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+    int n, i, flag=0;
+    scanf("%d",&n);
+    for(i=2;i<=n/2;++i)
+        if(n%i==0){flag=1;break;}
+        if (flag == 0)printf("yes");
+        else printf("no");
+    return 0;
+}
