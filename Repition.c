1570428b25287a52ecc5159be7709e2a20607af7
Manohar649 <37975082+Manohar649@users.noreply.github.com>
2019-03-24 07:11:42 +0530

    Create Repition.c

---
+++ Repition.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void){
+   int a[100],c,i,pro=0,n,j;
+ scanf("%d%d",&n,&j);
+ for(i=0;i<n;i++)scanf("%d",&a[i]);
+ for(i=0;i<n;i++)if(a[i]==j)pro++;
+ printf("%d",pro);
+ return 0;
+}
