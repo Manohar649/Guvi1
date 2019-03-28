03783f5f13d09a13b9a9e5a52a1e357eb2b1094e
Manohar649 <37975082+Manohar649@users.noreply.github.com>
2019-03-28 11:16:13 +0530

    Update Modulo_divided.c

--- Modulo_divided.c
+++ Modulo_divided.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-int main(void) {	
+long main(void) {	
 int n1,n2,i;
 char c;
-scanf("%d %c %d",&n1,&c,&n2);
-if(c=='/') printf("%d\n",(n1/n2));
-else printf("%d\n",(n1%n2));
+scanf("%ld %c %ld",&n1,&c,&n2);
+if(c=='/') printf("%ld\n",(n1/n2));
+else printf("%ld\n",(n1%n2));
 }
