443093b1f428fc3db114196eb6e82cb5551e82e2
Manohar649 <37975082+Manohar649@users.noreply.github.com>
2019-03-30 14:01:49 +0530

    Update Rightshift.c

--- Rightshift.c
+++ Rightshift.c
@@ -5,7 +5,10 @@ int main(void)
     scanf("%d%d",&n,&k);
     for(i=0;i<n;i++)scanf("%d",&a[i]);
     temp=n-k;
+    if(n!=1){
     for(i=temp;i<n;i++){b[j]=a[i];j++;}
     for(i=0;i<temp;i++){b[j]=a[i];j++;}
     for(j=0;j<n;j++)printf("%d ",b[j]);
+    }else printf("%d",a[0]);
 }
+
