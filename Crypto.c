b133e1107453d1a3c0a17e6cf755d651177167b0
Manohar649 <37975082+Manohar649@users.noreply.github.com>
2019-03-30 12:07:59 +0530

    Create Crypto.c

---
+++ Crypto.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+    int i,j;
+    char a[100];
+    scanf("%s",a);
+    j=strlen(a);
+    for(i=0;i<j;i++){
+    if(a[i]>=65&&a[i]<=87||a[i]>=97&&a[i]<=119){a[i]+=3;}
+    else if(a[i]==90||a[i]==122)a[i]-=23;
+    else if(a[i]==89||a[i]==121)a[i]-=23;
+    else if(a[i]==88||a[i]==120)a[i]-=23;}
+    printf("%s",a);
+    return 0;
+}
